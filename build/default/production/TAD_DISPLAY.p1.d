32cbeed3741798448cf9e295d4c37fe20920c46c
Gerard Pérez i Carbó <gerard.pc@students.salle.url.edu>
2025-03-28 21:14:48 +0100

    delete FIles

--- build/default/production/TAD_DISPLAY.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/TAD_DISPLAY.p1:  \
-TAD_DISPLAY.c  \
-TAD_TIMER.h  \
-TAD_DISPLAY.h 
--- build/default/production/TAD_TECLADO.p1.d
+++
@@ -1,6 +0,0 @@
-build/default/production/TAD_TECLADO.p1:  \
-TAD_TECLADO.c  \
-TAD_TECLADO.h  \
-TAD_TERMINAL.h  \
-TAD_DATOS.h  \
-TAD_TIMER.h 
